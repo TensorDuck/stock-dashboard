3c62c35f6b72c3e111573905a02cbf5f9a9f1401
TensorDuck <jchen978@gmail.com>
2021-04-25 15:15:40 -0700

    feat: add bollinger bands to the plot

--- stock_dashboard/app.py
+++ stock_dashboard/app.py
@@ -49,6 +49,36 @@ class StockInfo:
         """
         return self.prices["Close"].rolling(period).mean()
 
+    def bollinger_bands(self, period: int = 20, m_sigma: int = 2) -> pd.DataFrame:
+        """Calculate the bollinger bands for the security
+
+        These bands come in a pair of upper+lower bands that indicate how
+        volatile the stock is. When the bands grow narrow, the volatility is low. If the bands grow wider, then the volatility is high.
+
+        Bollinger bands is based off of the "Typical Price" (TP) that is computed as a
+        the average of the high, low and closing price for each day.
+
+        For more information, see:
+        https://www.investopedia.com/terms/b/bollingerbands.asp
+
+        Args:
+            period: The number of days to average over
+            m_sigma: Number of sigmas to plot the upper and lower bollinger bands.
+
+        Returns:
+            Dataframe containing "bollinger_ma", "bollinger_upper", "bollinger_lower"
+        """
+        typical_price = (
+            self.prices["Close"] + self.prices["High"] + self.prices["Low"]
+        ) / 3
+        moving_average = typical_price.rolling(period).mean()
+        moving_sd = typical_price.rolling(period).std()
+        df = moving_average.rename("bollinger_ma").to_frame()
+        df["bollinger_upper"] = moving_average + (m_sigma * moving_sd)
+        df["bollinger_lower"] = moving_average - (m_sigma * moving_sd)
+
+        return df
+
     def get_sub_prices_by_day(self, start_date: str, end_date: str) -> pd.DataFrame:
         """Get a sub-sample between the start and end dates"""
 
@@ -65,7 +95,7 @@ class StockInfo:
         sub_prices = self.get_sub_prices_by_day(start_date, end_date)
         min_date = min(sub_prices.index)
         max_date = max(sub_prices.index)
-        return (sub_prices["Close"][min_date] - sub_prices["Close"][max_date]) / (
+        return (sub_prices["Close"][max_date] - sub_prices["Close"][min_date]) / (
             sub_prices["Close"][min_date]
         )
 
@@ -213,6 +243,26 @@ def run_main():
                 y=stock_info.rolling_average(20)[prices.index],
                 mode="lines",
                 name="20-day Average",
+            )
+        )
+
+        # calculate bollinger bands
+        bollinger_bands = stock_info.bollinger_bands().loc[prices.index]
+        fig.add_trace(
+            pgo.Scatter(
+                x=bollinger_bands.index,
+                y=bollinger_bands["bollinger_upper"],
+                mode="lines",
+                name="bollinger_lower",
+            )
+        )
+        fig.add_trace(
+            pgo.Scatter(
+                x=bollinger_bands.index,
+                y=bollinger_bands["bollinger_lower"],
+                mode="lines",
+                name="bollinger_upper",
+                fill="tonexty",
             )
         )
         st.write(fig)  # write it to streamlit
